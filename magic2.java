02c85ab7432e2b4854c7b9c166e72afbd2b28a97
samrudhms <samrudh023@gmail.com>
2025-07-11 19:29:27 +0530

    v4

---
+++ magic2.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+class magic2{
+    public static void main(String[] args) {
+        ArrayList<String> A1=new ArrayList<>();
+        A1.add("pets");
+        A1.add("wild animals");
+        System.out.println(A1);
+        A1.remove(0);
+        System.out.println(A1);
+    }
+}
