44c5e7ba8eb3fe7965d25dae527153979fe5ce09
samrudhms <samrudh023@gmail.com>
2025-07-11 19:18:55 +0530

    v11

---
+++ levioasa.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+
+class levioasa{
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("lion");
+        animals.add("deer");
+        animals.add("tiger");
+        System.out.println("=============Printing Current Array==========");
+        System.out.println(animals);
+        animals.clear();
+        System.out.println("=============Printing Cleared Array==========");
+        System.out.println("Empty Array:animals");
+        if (animals.isEmpty()) {
+            System.out.println("Array is Empty");
+        }
+        else{
+            System.out.println("Array is not empty");
+        }
+        animals.add("lion");
+        animals.add("deer");
+        animals.add("tiger");
+    }
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
